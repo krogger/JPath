76f4b0a62e39d041f4c77ed8ac1b18d6ada05aa9
krog <krog@amazon.com>
2015-01-04 00:05:54 -0800

    Applying code style (getting it out of the way in a single commit to make future changes more visible)

--- JPath/src/to/us/harha/jpath/Engine.java
+++ JPath/src/to/us/harha/jpath/Engine.java
@@ -1,143 +1,143 @@
 package to.us.harha.jpath;
 
+import to.us.harha.jpath.tracer.Tracer;
+import to.us.harha.jpath.util.Logger;
+import to.us.harha.jpath.util.TimeUtils;
+
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import to.us.harha.jpath.tracer.Tracer;
-import to.us.harha.jpath.util.Logger;
-import to.us.harha.jpath.util.TimeUtils;
-
 public class Engine
 {
-	private int				m_cpu_cores;
-	private double			m_frameTime;
-	private boolean			m_isRunning;
-	private Display			m_display;
-	private Tracer			m_tracer;
-	private Logger			m_log;
-	private ExecutorService	m_eService;
+    private int m_cpu_cores;
+    private double m_frameTime;
+    private boolean m_isRunning;
+    private Display m_display;
+    private Tracer m_tracer;
+    private Logger m_log;
+    private ExecutorService m_eService;
 
-	public Engine(Display display, double frameTime)
-	{
-		m_cpu_cores = Runtime.getRuntime().availableProcessors();
+    public Engine(Display display, double frameTime)
+    {
+        m_cpu_cores = Runtime.getRuntime().availableProcessors();
 
-		m_log = new Logger(this.getClass().getName());
-		m_log.printMsg("Engine instance has been started! # of Available CPU Cores: " + m_cpu_cores);
+        m_log = new Logger(this.getClass().getName());
+        m_log.printMsg("Engine instance has been started! # of Available CPU Cores: " + m_cpu_cores);
 
-		m_frameTime = 1.0 / frameTime;
-		m_isRunning = false;
-		m_display = display;
-		m_tracer = new Tracer(m_display, 4, m_cpu_cores);
-		m_eService = Executors.newFixedThreadPool(m_cpu_cores);
+        m_frameTime = 1.0 / frameTime;
+        m_isRunning = false;
+        m_display = display;
+        m_tracer = new Tracer(m_display, 4, m_cpu_cores);
+        m_eService = Executors.newFixedThreadPool(m_cpu_cores);
 
-	}
+    }
 
-	public void start()
-	{
-		if (m_isRunning)
-			return;
+    public void start()
+    {
+        if (m_isRunning)
+            return;
 
-		m_isRunning = true;
-		run();
-	}
+        m_isRunning = true;
+        run();
+    }
 
-	public void stop()
-	{
-		if (!m_isRunning)
-			return;
+    public void stop()
+    {
+        if (!m_isRunning)
+            return;
 
-		m_isRunning = false;
-	}
+        m_isRunning = false;
+    }
 
-	private void run()
-	{
-		int frames = 0;
-		double frameCounter = 0;
+    private void run()
+    {
+        int frames = 0;
+        double frameCounter = 0;
 
-		double lastTime = TimeUtils.getTime();
-		double unprocessedTime = 0.0;
+        double lastTime = TimeUtils.getTime();
+        double unprocessedTime = 0.0;
 
-		while (m_isRunning)
-		{
-			boolean render = false;
+        while (m_isRunning)
+        {
+            boolean render = false;
 
-			double startTime = TimeUtils.getTime();
-			double passedTime = startTime - lastTime;
-			lastTime = startTime;
+            double startTime = TimeUtils.getTime();
+            double passedTime = startTime - lastTime;
+            lastTime = startTime;
 
-			unprocessedTime += passedTime;
-			frameCounter += passedTime;
+            unprocessedTime += passedTime;
+            frameCounter += passedTime;
 
-			while (unprocessedTime > m_frameTime)
-			{
-				render = true;
-				unprocessedTime -= m_frameTime;
+            while (unprocessedTime > m_frameTime)
+            {
+                render = true;
+                unprocessedTime -= m_frameTime;
 
-				update((float) m_frameTime);
+                update((float) m_frameTime);
 
-				if (frameCounter >= 1.0)
-				{
-					// FPS INFO HERE
-					frames = 0;
-					frameCounter = 0;
-				}
-			}
+                if (frameCounter >= 1.0)
+                {
+                    // FPS INFO HERE
+                    frames = 0;
+                    frameCounter = 0;
+                }
+            }
 
-			if (render)
-			{
-				render();
-				frames++;
-			} else
-			{
-				try
-				{
-					Thread.sleep(1);
-				} catch (InterruptedException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+            if (render)
+            {
+                render();
+                frames++;
+            } else
+            {
+                try
+                {
+                    Thread.sleep(1);
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	private void update(float delta)
-	{
-		m_tracer.update(delta);
-	}
+    private void update(float delta)
+    {
+        m_tracer.update(delta);
+    }
 
-	private void render()
-	{
-		BufferStrategy bs = m_display.getBufferStrategy();
-		if (bs == null)
-		{
-			m_display.createBufferStrategy(2);
-			return;
-		}
+    private void render()
+    {
+        BufferStrategy bs = m_display.getBufferStrategy();
+        if (bs == null)
+        {
+            m_display.createBufferStrategy(2);
+            return;
+        }
 
-		m_tracer.incrementSampleCounter();
+        m_tracer.incrementSampleCounter();
 
-		// This is just some quick code, everything will be changed and fixed, it's just really late atm
-		// and I want to get something that works at least in some way
-		if (m_cpu_cores < 4)
-		{
-			m_tracer.render(m_display);
-		} else
-		{
-			for (int j = 0; j < m_cpu_cores / 2; j++)
-			{
-				for (int i = 0; i < m_cpu_cores / 2; i++)
-				{
-					m_tracer.renderPortion(m_display, i, j);
-				}
-			}
-		}
+        // This is just some quick code, everything will be changed and fixed, it's just really late atm
+        // and I want to get something that works at least in some way
+        if (m_cpu_cores < 4)
+        {
+            m_tracer.render(m_display);
+        } else
+        {
+            for (int j = 0; j < m_cpu_cores / 2; j++)
+            {
+                for (int i = 0; i < m_cpu_cores / 2; i++)
+                {
+                    m_tracer.renderPortion(m_display, i, j);
+                }
+            }
+        }
 
-		Graphics g = bs.getDrawGraphics();
-		g.drawImage(m_display.getImage(), 0, 0, m_display.getWidth() * m_display.getScale(), m_display.getHeight() * m_display.getScale(), null);
-		g.dispose();
-		bs.show();
-	}
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(m_display.getImage(), 0, 0, m_display.getWidth() * m_display.getScale(), m_display.getHeight() * m_display.getScale(), null);
+        g.dispose();
+        bs.show();
+    }
 
 }
--- JPath/src/to/us/harha/jpath/tracer/Tracer.java
+++ JPath/src/to/us/harha/jpath/tracer/Tracer.java
@@ -1,8 +1,5 @@
 package to.us.harha.jpath.tracer;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import to.us.harha.jpath.Display;
 import to.us.harha.jpath.tracer.object.Material;
 import to.us.harha.jpath.tracer.object.TracerObject;
@@ -12,197 +9,200 @@ import to.us.harha.jpath.util.math.Primitive;
 import to.us.harha.jpath.util.math.Ray;
 import to.us.harha.jpath.util.math.Vec3f;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Tracer
 {
 
-	private Display			m_display;
-	private int				m_maxrecursion;
-	private int				m_cpu_cores;
-	private Vec3f[]			m_samples;
-	private AtomicInteger	m_samples_taken;
-	private Scene			m_scene;
-	private Logger			m_log;
+    private Display m_display;
+    private int m_maxrecursion;
+    private int m_cpu_cores;
+    private Vec3f[] m_samples;
+    private AtomicInteger m_samples_taken;
+    private Scene m_scene;
+    private Logger m_log;
 
-	public Tracer(Display display, int maxrecursion, int cpu_cores)
-	{
-		m_display = display;
-		m_maxrecursion = maxrecursion;
-		m_cpu_cores = cpu_cores;
-		m_samples = new Vec3f[display.getWidth() * display.getHeight()];
-		m_samples_taken = new AtomicInteger();
-		m_scene = new Scene();
-		m_log = new Logger(this.getClass().getName());
+    public Tracer(Display display, int maxrecursion, int cpu_cores)
+    {
+        m_display = display;
+        m_maxrecursion = maxrecursion;
+        m_cpu_cores = cpu_cores;
+        m_samples = new Vec3f[display.getWidth() * display.getHeight()];
+        m_samples_taken = new AtomicInteger();
+        m_scene = new Scene();
+        m_log = new Logger(this.getClass().getName());
 
-		clearSamples();
+        clearSamples();
 
-		m_log.printMsg("Tracer instance has been started, using " + m_cpu_cores + " CPU Cores!");
-	}
+        m_log.printMsg("Tracer instance has been started, using " + m_cpu_cores + " CPU Cores!");
+    }
 
-	public void update(float delta)
-	{
+    public void update(float delta)
+    {
 
-	}
+    }
 
-	// Single threaded rendering
-	public void render(Display display)
-	{
-		float width = display.getWidth();
-		float height = display.getHeight();
+    // Single threaded rendering
+    public void render(Display display)
+    {
+        float width = display.getWidth();
+        float height = display.getHeight();
 
-		Ray ray = new Ray(new Vec3f(0.0f, 2.5f, 13.0f), new Vec3f(0.0f, 0.0f, -1.0f));
-		for (int y = 0; y < display.getHeight(); y++)
-		{
-			for (int x = 0; x < display.getWidth(); x++)
-			{
-				int index = x + y * display.getWidth();
+        Ray ray = new Ray(new Vec3f(0.0f, 2.5f, 13.0f), new Vec3f(0.0f, 0.0f, -1.0f));
+        for (int y = 0; y < display.getHeight(); y++)
+        {
+            for (int x = 0; x < display.getWidth(); x++)
+            {
+                int index = x + y * display.getWidth();
 
-				float x_norm = (x - width * 0.5f) / width * display.getAR();
-				float y_norm = (height * 0.5f - y) / height;
-				ray.setDir(Vec3f.normalize(new Vec3f(x_norm, y_norm, -1.0f)));
+                float x_norm = (x - width * 0.5f) / width * display.getAR();
+                float y_norm = (height * 0.5f - y) / height;
+                ray.setDir(Vec3f.normalize(new Vec3f(x_norm, y_norm, -1.0f)));
 
-				m_samples[index] = Vec3f.add(m_samples[index], pathTrace(ray, 0));
+                m_samples[index] = Vec3f.add(m_samples[index], pathTrace(ray, 0));
 
-				display.drawPixelVec3fAveraged(index, m_samples[index], m_samples_taken.get());
-			}
-		}
-	}
+                display.drawPixelVec3fAveraged(index, m_samples[index], m_samples_taken.get());
+            }
+        }
+    }
 
-	// Multithreaded rendering
-	public void renderPortion(Display display, int t1, int t2)
-	{
-		int t = t1 + t2 * (m_cpu_cores / 2);
+    // Multithreaded rendering
+    public void renderPortion(Display display, int t1, int t2)
+    {
+        int t = t1 + t2 * (m_cpu_cores / 2);
 
-		if (t1 >= (m_cpu_cores / 2))
-			t1 = (m_cpu_cores / 2) - 1;
-		if (t2 >= (m_cpu_cores / 2))
-			t2 = (m_cpu_cores / 2) - 1;
+        if (t1 >= (m_cpu_cores / 2))
+            t1 = (m_cpu_cores / 2) - 1;
+        if (t2 >= (m_cpu_cores / 2))
+            t2 = (m_cpu_cores / 2) - 1;
 
-		float width = display.getWidth();
-		float height = display.getHeight();
-		int width_portion = display.getWidth() / (m_cpu_cores / 2);
-		int height_portion = display.getHeight() / (m_cpu_cores / 2);
+        float width = display.getWidth();
+        float height = display.getHeight();
+        int width_portion = display.getWidth() / (m_cpu_cores / 2);
+        int height_portion = display.getHeight() / (m_cpu_cores / 2);
 
-		Ray ray = new Ray(new Vec3f(0.0f, 2.5f, 13.0f), new Vec3f(0.0f, 0.0f, -1.0f));
+        Ray ray = new Ray(new Vec3f(0.0f, 2.5f, 13.0f), new Vec3f(0.0f, 0.0f, -1.0f));
 
-		for (int y = height_portion * t2; y < (height_portion * t2) + height_portion; y++)
-		{
-			for (int x = width_portion * t1; x < (width_portion * t1) + width_portion; x++)
-			{
-				int xx = x - width_portion * t1;
-				int yy = y - height_portion * t2;
-				int index_screen = x + y * display.getWidth();
-				int index_sample = xx + yy * width_portion;
+        for (int y = height_portion * t2; y < (height_portion * t2) + height_portion; y++)
+        {
+            for (int x = width_portion * t1; x < (width_portion * t1) + width_portion; x++)
+            {
+                int xx = x - width_portion * t1;
+                int yy = y - height_portion * t2;
+                int index_screen = x + y * display.getWidth();
+                int index_sample = xx + yy * width_portion;
 
-				if (xx == 0 || xx == width_portion || yy == 0 || yy == width_portion)
-				{
-					// For debugging purposes, leave the borders of a rendered portion unrendered
-				} else
-				{
-					float x_norm = (x - width * 0.5f) / width * display.getAR();
-					float y_norm = (height * 0.5f - y) / height;
-					ray.setDir(Vec3f.normalize(new Vec3f(x_norm, y_norm, -1.0f)));
+                if (xx == 0 || xx == width_portion || yy == 0 || yy == width_portion)
+                {
+                    // For debugging purposes, leave the borders of a rendered portion unrendered
+                } else
+                {
+                    float x_norm = (x - width * 0.5f) / width * display.getAR();
+                    float y_norm = (height * 0.5f - y) / height;
+                    ray.setDir(Vec3f.normalize(new Vec3f(x_norm, y_norm, -1.0f)));
 
-					m_samples[index_screen] = Vec3f.add(m_samples[index_screen], pathTrace(ray, 0));
+                    m_samples[index_screen] = Vec3f.add(m_samples[index_screen], pathTrace(ray, 0));
 
-					display.drawPixelVec3fAveraged(index_screen, m_samples[index_screen], m_samples_taken.get());
-				}
+                    display.drawPixelVec3fAveraged(index_screen, m_samples[index_screen], m_samples_taken.get());
+                }
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	/*
-	 * Path tracing
-	 * n = recursion level
-	 */
-	public Vec3f pathTrace(Ray ray, int n)
-	{
-		// Return black if max recursion depth has been exceeded
-		if (n > m_maxrecursion)
-			return new Vec3f();
+    /*
+     * Path tracing
+     * n = recursion level
+     */
+    public Vec3f pathTrace(Ray ray, int n)
+    {
+        // Return black if max recursion depth has been exceeded
+        if (n > m_maxrecursion)
+            return new Vec3f();
 
-		// Initialize some objects and variables
-		Vec3f color_final = new Vec3f();
-		Intersection iSection = null;
-		Intersection iSectionFinal = null;
-		TracerObject OBJECT = null;
-		float t_init = Float.MAX_VALUE;
+        // Initialize some objects and variables
+        Vec3f color_final = new Vec3f();
+        Intersection iSection = null;
+        Intersection iSectionFinal = null;
+        TracerObject OBJECT = null;
+        float t_init = Float.MAX_VALUE;
 
-		// Intersect the initial ray against all scene objects and find the closest interestection to the ray origin
-		for (TracerObject o : m_scene.getObjects())
-		{
-			for (Primitive p : o.getPrimitives())
-			{
-				iSection = p.intersect(ray);
-				if (iSection != null)
-				{
-					if (iSection.getT() < t_init)
-					{
-						iSectionFinal = iSection;
-						t_init = iSection.getT();
-						OBJECT = o;
-					}
-				}
-			}
-		}
+        // Intersect the initial ray against all scene objects and find the closest interestection to the ray origin
+        for (TracerObject o : m_scene.getObjects())
+        {
+            for (Primitive p : o.getPrimitives())
+            {
+                iSection = p.intersect(ray);
+                if (iSection != null)
+                {
+                    if (iSection.getT() < t_init)
+                    {
+                        iSectionFinal = iSection;
+                        t_init = iSection.getT();
+                        OBJECT = o;
+                    }
+                }
+            }
+        }
 
-		// If no intersection happened at all, return black
-		if (iSectionFinal == null)
-			return new Vec3f();
+        // If no intersection happened at all, return black
+        if (iSectionFinal == null)
+            return new Vec3f();
 
-		// Get the object's surface material
-		Material M = OBJECT.getMaterial();
+        // Get the object's surface material
+        Material M = OBJECT.getMaterial();
 
-		// If the object is a light source, return it's emittance
-		if (Vec3f.length(M.getEmittance()) > 0.0f)
-			return M.getEmittance();
+        // If the object is a light source, return it's emittance
+        if (Vec3f.length(M.getEmittance()) > 0.0f)
+            return M.getEmittance();
 
-		// If the object is reflective like a mirror, reflect a ray
-		if (M.getReflectivity() > 0.0f)
-		{
-			Ray newRay;
-			if (M.getGlossiness() > 0.0f)
-				newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.add(Vec3f.reflect(ray.getDir(), iSectionFinal.getNorm()), Vec3f.scale(Vec3f.randomHemisphere(iSectionFinal.getNorm()), M.getGlossiness()))));
-			else
-				newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.reflect(ray.getDir(), iSectionFinal.getNorm())));
-			color_final = Vec3f.add(color_final, Vec3f.scale(pathTrace(newRay, n + 1), M.getReflectivity()));
-		}
+        // If the object is reflective like a mirror, reflect a ray
+        if (M.getReflectivity() > 0.0f)
+        {
+            Ray newRay;
+            if (M.getGlossiness() > 0.0f)
+                newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.add(Vec3f.reflect(ray.getDir(), iSectionFinal.getNorm()), Vec3f.scale(Vec3f.randomHemisphere(iSectionFinal.getNorm()), M.getGlossiness()))));
+            else
+                newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.reflect(ray.getDir(), iSectionFinal.getNorm())));
+            color_final = Vec3f.add(color_final, Vec3f.scale(pathTrace(newRay, n + 1), M.getReflectivity()));
+        }
 
-		// If the object is refractive like glass, refract the ray
-		if (M.getRefractivity() > 0.0f)
-		{
-			Ray newRay;
-			if (M.getGlossiness() > 0.0f)
-				newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.add(Vec3f.refract(ray.getDir(), iSectionFinal.getNorm(), 1.0f, M.getRefractivityIndex()), Vec3f.scale(Vec3f.randomHemisphere(iSectionFinal.getNorm()), M.getGlossiness()))));
-			else
-				newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.refract(ray.getDir(), iSectionFinal.getNorm(), 1.0f, M.getRefractivityIndex())));
-			color_final = Vec3f.add(color_final, Vec3f.scale(pathTrace(newRay, n + 1), M.getRefractivity()));
-		}
+        // If the object is refractive like glass, refract the ray
+        if (M.getRefractivity() > 0.0f)
+        {
+            Ray newRay;
+            if (M.getGlossiness() > 0.0f)
+                newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.add(Vec3f.refract(ray.getDir(), iSectionFinal.getNorm(), 1.0f, M.getRefractivityIndex()), Vec3f.scale(Vec3f.randomHemisphere(iSectionFinal.getNorm()), M.getGlossiness()))));
+            else
+                newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.refract(ray.getDir(), iSectionFinal.getNorm(), 1.0f, M.getRefractivityIndex())));
+            color_final = Vec3f.add(color_final, Vec3f.scale(pathTrace(newRay, n + 1), M.getRefractivity()));
+        }
 
-		// Calculate the diffuse lighting if reflectance is greater than 0.0
-		// NOTE: This could be improved / changed, it isn't physically correct at all atm and it's quite simple
-		if (Vec3f.length(M.getReflectance()) > 0.0f)
-		{
-			Ray newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.randomHemisphere(iSectionFinal.getNorm())));
+        // Calculate the diffuse lighting if reflectance is greater than 0.0
+        // NOTE: This could be improved / changed, it isn't physically correct at all atm and it's quite simple
+        if (Vec3f.length(M.getReflectance()) > 0.0f)
+        {
+            Ray newRay = new Ray(iSectionFinal.getPos(), Vec3f.normalize(Vec3f.randomHemisphere(iSectionFinal.getNorm())));
 
-			float NdotD = Math.abs(Vec3f.dot(iSectionFinal.getNorm(), newRay.getDir()));
-			Vec3f BRDF = Vec3f.scale(M.getReflectance(), 1.0f * NdotD);
-			Vec3f REFLECTED = pathTrace(newRay, n + 1);
+            float NdotD = Math.abs(Vec3f.dot(iSectionFinal.getNorm(), newRay.getDir()));
+            Vec3f BRDF = Vec3f.scale(M.getReflectance(), 1.0f * NdotD);
+            Vec3f REFLECTED = pathTrace(newRay, n + 1);
 
-			color_final = Vec3f.add(color_final, Vec3f.add(M.getEmittance(), Vec3f.scale(BRDF, REFLECTED)));
-		}
+            color_final = Vec3f.add(color_final, Vec3f.add(M.getEmittance(), Vec3f.scale(BRDF, REFLECTED)));
+        }
 
-		return color_final;
-	}
+        return color_final;
+    }
 
-	public void clearSamples()
-	{
-		Arrays.fill(m_samples, new Vec3f());
-	}
+    public void clearSamples()
+    {
+        Arrays.fill(m_samples, new Vec3f());
+    }
 
-	public void incrementSampleCounter()
-	{
-		m_samples_taken.incrementAndGet();
-	}
+    public void incrementSampleCounter()
+    {
+        m_samples_taken.incrementAndGet();
+    }
 
 }
